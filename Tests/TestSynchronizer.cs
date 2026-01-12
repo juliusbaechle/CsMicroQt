f9b2893fb5911fc5b9a588f3472932d77b97bf0a
Julius Bächle <juliusbaechle@users.noreply.github.com>
2026-01-12 09:59:43 +0100

    Split tests for timer and synchronizer

---
+++ Tests/TestSynchronizer.cs
@@ -0,0 +1,22 @@
+﻿using MicroQt;
+
+namespace MicroQtTests {
+    [TestClass]
+    public sealed class TestSynchronizer {
+
+        [TestMethod]
+        public void Test() {
+            EventLoop eventLoop = new();
+            Synchronizer synchronizer = new(eventLoop);
+            eventLoop.EnqueueEvent(() => {
+                eventLoop.EnqueueEvent(() => {
+                    synchronizer.Exit(1);
+                });
+
+                int i = synchronizer.Exec();
+                eventLoop.Exit(i);
+            });
+            Assert.AreEqual(1, eventLoop.Exec());
+        }
+    }
+}
---
+++ Tests/TestTimer.cs
@@ -0,0 +1,33 @@
+﻿using MicroQt;
+
+namespace MicroQtTests {
+    [TestClass]
+    public sealed class TestTimer {
+        [TestMethod]
+        public void Test() {
+            EventLoop eventLoop = new();
+
+            MicroQt.Timer timer1 = new(eventLoop);
+            timer1.Elapsed += () => { 
+                timer1.Dispose(); 
+                eventLoop.Exit(1); 
+            };
+            timer1.Start(140);
+            timer1.SingleShot = true;
+
+            uint counter = 0;
+            MicroQt.Timer timer2 = new(eventLoop, 50);
+            timer2.Elapsed += () => {
+                counter++;
+                if (timer1.Active)
+                    timer1.Stop();
+                else
+                    timer1.Resume();
+            };
+            timer2.Start();
+
+            eventLoop.Exec();
+            Assert.AreEqual((uint) 4, counter);
+        }
+    }
+}
